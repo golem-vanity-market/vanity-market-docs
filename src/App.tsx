63af2f4aa0502233e602e5ff1795b54017765b1f
scx1332 <sieciech@gmail.com>
2025-11-03 15:14:39 +0100

    Working excalidraw

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import {Excalidraw} from "@excalidraw/excalidraw"; // your saved Excalidraw data
 import "@excalidraw/excalidraw/index.css";
+import jsonData from "./example.excalidraw.json"
+import {ExcalidrawInitialDataState} from "@excalidraw/excalidraw/types";
+
+
 
 function App() {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
       <div style={{ height: "500px" }}>
-        <Excalidraw />
+        <Excalidraw
+          initialData={jsonData as unknown as ExcalidrawInitialDataState}
+          viewModeEnabled={true}
+        />
       </div>
     </>
   );
