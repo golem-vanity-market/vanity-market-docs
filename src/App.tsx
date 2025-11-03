7d20dc292c9fbfdfb2aa84b881d9cc3f3e8ddc14
scx1332 <sieciech@gmail.com>
2025-11-03 15:10:12 +0100

    Working excalidraw

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./index.css";
+import React from "react";
+import {Excalidraw} from "@excalidraw/excalidraw"; // your saved Excalidraw data
+import "@excalidraw/excalidraw/index.css";
 
-export function App() {
-  return <div className="app">App</div>;
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
+      <div style={{ height: "500px" }}>
+        <Excalidraw />
+      </div>
+    </>
+  );
 }
 
 export default App;
